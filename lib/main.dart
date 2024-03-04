8e32c864dfc5e49bc843b02800dc81c9df2c52eb
lolopindik <dimaluka2014@gmail.com>
2024-03-04 22:29:04 +0300

    screen rotation is blocked due to the fact that when you rotate the screen on phones, the height becomes too small, which leads to user interface errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'pages/grid_cards.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(const MyApp());
+  });
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
   @override
