01ffe06a2703741bfbdcd314ca98c37e8e8f605f
lolopindik <dimaluka2014@gmail.com>
2024-03-14 12:37:42 +0300

    supabaseUrl and supabaseAnnonKey added into Constants class

---
+++ lib/modules/constants.dart
@@ -0,0 +1,4 @@
+abstract class Constants {
+  static const String supabaseUrl = 'https://nkcjhwcuebcqbvumcism.supabase.co';
+  static const String supabaseAnnonKey =  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rY2pod2N1ZWJjcWJ2dW1jaXNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk2MjM2MDAsImV4cCI6MjAyNTE5OTYwMH0.DrND9DtkAvz5Ep5WmD6OWmYffffAW_b0O5AovLEZ0uY';
+}
